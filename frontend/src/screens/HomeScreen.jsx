49ba8934acd8d89a90cee195944c89d96a644d6e
karen9999 <karen.dev999@gmail.com>
2021-04-06 17:21:51 +0300

    created screens

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import Product from '../components/Product';
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/products').then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+  return (
+    <>
+      <h1>Latest products</h1>
+      <Row>
+        {products.map((prod) => {
+          return (
+            <Col sm={12} lg={4} xl={3} md={6}>
+              <Product product={prod} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
