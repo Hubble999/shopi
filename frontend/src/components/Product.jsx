9bdb3bb3d249116dd1ebf18ebd7ba0822407acff
karen9999 <karen.dev999@gmail.com>
2021-04-06 17:10:53 +0300

    created Product component

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import Rating from '../components/Rating';
+
+const Product = ({ product }) => {
+  const { name, image, brand, price, rating, _id } = product;
+  return (
+    <Card className='my-3 rounded'>
+      <LinkContainer to={`/product/${_id}`} style={{ padding: '0px' }}>
+        <Link>
+          <Card.Img variant='top' src={image} />
+        </Link>
+      </LinkContainer>
+      <Card.Body>
+        <Card.Title>
+          <strong>{name}</strong>
+        </Card.Title>
+        <Card.Text>{brand}</Card.Text>
+        <Rating value={rating} />
+        <LinkContainer to='/product'>
+          <Button className='my-2'>${price}</Button>
+        </LinkContainer>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
