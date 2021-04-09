17104f63f61400afaef3a2630304214052306461
karen9999 <karen.dev999@gmail.com>
2021-04-09 13:09:22 +0300

    created Warning component

---
+++ frontend/src/components/Warning.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+
+function Warning({ error }) {
+  return <Alert variant='danger'>{error}</Alert>;
+}
+
+export default Warning;
