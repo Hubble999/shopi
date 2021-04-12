d1780498ff43a3eb75d64c65fe9c43455b9823db
karen9999 <karen.dev999@gmail.com>
2021-04-12 14:15:11 +0300

    created action addCart

---
+++ frontend/src/actions/actionAddCart.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export default (id, amount) => (dispatch, getState) => {
+  axios.get(`/api/products/${id}`).then((res) => {
+    const { data } = res;
+    const { _id, name, countInStock, image, price } = data;
+    dispatch({
+      type: 'ADD_CART',
+      payload: {
+        item: {
+          id: _id,
+          name,
+          countInStock,
+          image,
+          price,
+          amount,
+        },
+      },
+    });
+    const { cartItems } = getState().cart;
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  });
+};
