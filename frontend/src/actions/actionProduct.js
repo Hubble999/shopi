0f9ebbc0ffee9452595e27d5ed65642919da32ec
karen9999 <karen.dev999@gmail.com>
2021-04-12 14:26:05 +0300

    fix style code

--- frontend/src/actions/actionProduct.js
+++ frontend/src/actions/actionProduct.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 
 export default (id) => (dispatch) => {
-  dispatch({ type: 'PRODUCT_REQUEST' });
+  dispatch({ type: 'PRODUCT_DETAILS_REQUEST' });
 
-  axios.get(`/api/products/${id}`)
+  axios
+    .get(`/api/products/${id}`)
     .then((res) => {
-      dispatch({ type: 'PRODUCT_SUCCESS', payload: { data: res.data } });
+      dispatch({
+        type: 'PRODUCT_DETAILS_SUCCESS',
+        payload: { data: res.data },
+      });
     })
     .catch((err) => {
       console.log(err.message);
-      dispatch({ type: 'PRODUCT_FAILURE', payload: { error: err.message } });
+      dispatch({
+        type: 'PRODUCT_DETAILS_FAILURE',
+        payload: { error: err.message },
+      });
     });
 };
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -3,14 +3,14 @@ export default (
   action,
 ) => {
   switch (action.type) {
-    case 'PRODUCT_REQUEST': {
+    case 'PRODUCT_DETAILS_REQUEST': {
       return { ...state, loading: true };
     }
-    case 'PRODUCT_SUCCESS': {
+    case 'PRODUCT_DETAILS_SUCCESS': {
       const { data } = action.payload;
       return { ...state, data, loading: false };
     }
-    case 'PRODUCT_FAILURE': {
+    case 'PRODUCT_DETAILS_FAILURE': {
       const { error } = action.payload;
       return { state, error };
     }
