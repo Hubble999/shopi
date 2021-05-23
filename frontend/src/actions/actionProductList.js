f66ee0a2a4e268c33cf168a793deeb1a2aa819a2
karen9999 <karen.dev999@gmail.com>
2021-05-23 23:05:45 +0300

    refactoring action / add handler error

--- frontend/src/actions/actionProductList.js
+++ frontend/src/actions/actionProductList.js
@@ -1,14 +1,26 @@
 import axios from 'axios';
 
-const productsListAction = () => (dispatch) => {
-  dispatch({ type: 'PRODUCTS_LIST_REQUEST' });
+const productsListAction =
+  (keyword = '') =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: 'PRODUCTS_LIST_REQUEST' });
 
-  axios.get('/api/products').then((res) => {
-    dispatch({
-      type: 'PRODUCTS_LIST_SUCCESS',
-      payload: { data: res.data },
-    });
-  });
-};
+      const { data } = await axios.get(`/api/products?keyword=${keyword}`);
+
+      dispatch({
+        type: 'PRODUCTS_LIST_SUCCESS',
+        payload: { data }
+      });
+    } catch (error) {
+      dispatch({
+        type: 'PRODUCTS_LIST_FAIL',
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+      });
+    }
+  };
 
 export default productsListAction;
