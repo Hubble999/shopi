406769c8d84d1f32d661ea539ab70f5c6a4777ff
karen9999 <karen.dev999@gmail.com>
2021-05-23 23:09:21 +0300

    add route for user login / registration

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import { authUser, profileUser, createUser } from '../controllers/userController.js';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js';
 import protectMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
-router.route('/profile').get(protectMiddleware, profileUser);
-router.route('/').post(createUser);
+router.route('/profile').get(protectMiddleware, getUserProfile);
+router.route('/').post(registerUser);
 
 export default router;
