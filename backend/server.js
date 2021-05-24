4743279ffacc73ef837294d1f4b8c975d203e980
karen9999 <karen.dev999@gmail.com>
2021-05-24 11:31:47 +0300

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import database from './config/database.js';
 import productRoutes from './routes/productRoutes.js';
@@ -13,11 +14,21 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+const __dirname = path.resolve();
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.listen(PORT, () => console.log('listen 5000'));
