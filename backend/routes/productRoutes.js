f7bf751e8112034a4f244d81238165c2cf07f8d7
karen9999 <karen.dev999@gmail.com>
2021-05-24 09:16:42 +0300

    add route for get top products from Carousel component

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {
   getProducts,
   getProductById,
+  getTopProducts
 } from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.get('/', getProducts);
+router.get('/top', getTopProducts);
 router.get('/:id', getProductById);
+router.get('/', getProducts);
 
 export default router;
